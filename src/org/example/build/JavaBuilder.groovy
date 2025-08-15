26713e10d3988920d76fcf8f3648d9422592cbcb
Simrankaurbumrah <104462059+Simrankaurbumrah@users.noreply.github.com>
2025-08-15 23:02:55 +0530

    Update JavaBuilder.groovy

--- src/org/example/build/JavaBuilder.groovy
+++ src/org/example/build/JavaBuilder.groovy
@@ -3,6 +3,5 @@ package org.example.build
 class JavaBuilder implements Builder {
     void buildScript(steps) {
         steps.echo "Running Maven build for Java project..."
-        steps.sh "mvn clean install"
     }
 }
