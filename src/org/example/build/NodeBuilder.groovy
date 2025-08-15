5818cdabfdef75a2904ae9781f1eb9a5e0428adb
Simrankaurbumrah <104462059+Simrankaurbumrah@users.noreply.github.com>
2025-08-15 23:27:33 +0530

    Update NodeBuilder.groovy

--- src/org/example/build/NodeBuilder.groovy
+++ src/org/example/build/NodeBuilder.groovy
@@ -3,8 +3,8 @@ package org.example.build
 class NodeBuilder implements Builder {
     void buildScript(steps) {
         steps.echo "Installing Node.js dependencies..."
-        steps.sh "npm install"
+        //steps.sh "npm install"
         steps.echo "Running Node.js build..."
-        steps.sh "npm run build"
+        //steps.sh "npm run build"
     }
 }
